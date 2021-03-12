0401712ce01d101826d2ef57c0da5bad4e64d586
vasyalee <flightsho@gmail.com>
2021-03-13 00:10:25 +0300

    Simplified code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import time
 from functions import *
 
@@ -9,8 +8,7 @@ while True:
     print(f'Осталось {tries} попыток')
     user_input = input('Введите число: ')
     user_input = int(user_input)
-    if is_valid(user_input, n):
-        if tries > 1:
+    if is_valid(user_input, n) and tries > 1:
             if user_input < generated:
                 print('Ваше число меньше загаданного, попробуйте еще разок')
                 tries -= 1
@@ -25,15 +23,15 @@ while True:
                     continue
                 elif again == 'нет':
                     break
-        else:
-            print('Упс, попытки закончились.')
-            again = check_again()
-            if again == 'да':
-                n, generated, tries = play_again()
-                continue
-            elif again == 'нет':
-                break
-    else:
+    elif tries <= 1:
+        print('Упс, попытки закончились.')
+        again = check_again()
+        if again == 'да':
+            n, generated, tries = play_again()
+            continue
+        elif again == 'нет':
+            break
+    elif not(is_valid(user_input, n)):
         print(f'А может быть все-таки введем целое число от 1 до {n}?')
         tries -= 1
         continue
